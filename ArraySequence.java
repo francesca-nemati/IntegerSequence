2ba7caba6d4d2781db477e5cbfb88898532de5e1
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-12-07 14:09:34 -0500

    added in methods defined in IntegerSequence, and altered them from their use in Range to suit ArraySequence

--- ArraySequence.java
+++ ArraySequence.java
@@ -5,5 +5,27 @@ public class ArraySequence implements IntegerSequence {
 
   public ArraySequence(int[] other) {
     data = other;
+    currentIndex = 0;
   }
+
+  public void reset() {
+    currentIndex = 0;
+  }
+  public int length() {
+    int len = data.length;
+    return len;
+  }
+  public boolean hasNext() {
+    if (currentIndex < length()-1) return true;
+    else return false;
+  }
+
+  //@throws NoSuchElementException
+  public int next() {
+    if (hasNext() == false) throw new NoSuchElementException ("There is no next value");
+    int curr = data[currentIndex];
+    currentIndex++;
+    return curr;
+  }
+
 }
